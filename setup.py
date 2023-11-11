d561bbcf69e28aec59b44db8ab1250268371435e
anvie <robinsyihab@gmail.com>
2023-11-11 21:11:20 +0700

    bump update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='envrypt',
-    version='0.0.1',
+    version='0.0.2',
     author='Robin Syihab',
     license='MIT',
     author_email='robinsyihab@gmail.com',
