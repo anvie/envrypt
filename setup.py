b4fed0b4ccdac1224d97c7ed042cbb40dbdf62ef
anvie <robinsyihab@gmail.com>
2023-11-11 21:04:05 +0700

    add license in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='envrypt',
     version='0.0.1',
     author='Robin Syihab',
+    license='MIT',
     author_email='robinsyihab@gmail.com',
     description='Python tool/library designed to secure environment variables by encrypting them both in disk and memory.',
     packages=find_packages(),
